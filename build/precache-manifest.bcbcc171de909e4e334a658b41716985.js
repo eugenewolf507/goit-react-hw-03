cbd534b785182bb6259aa0967c752b9030160a3a
Eugene Volkov <eugenewolf507@gmail.com>
2019-08-21 14:47:16 +0300

    load build

---
+++ build/precache-manifest.bcbcc171de909e4e334a658b41716985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d117a5e92b7c3546d267e5b82ff505",
+    "url": "/goit-react-hw-03//build/index.html"
+  },
+  {
+    "revision": "d7bb2e77e03eadfdfbc0",
+    "url": "/goit-react-hw-03//build/static/css/main.c19f4098.chunk.css"
+  },
+  {
+    "revision": "33c3251beeea8074ff28",
+    "url": "/goit-react-hw-03//build/static/js/2.16836a71.chunk.js"
+  },
+  {
+    "revision": "d7bb2e77e03eadfdfbc0",
+    "url": "/goit-react-hw-03//build/static/js/main.4f679ee5.chunk.js"
+  },
+  {
+    "revision": "a519c6a39cf4f82ebef3",
+    "url": "/goit-react-hw-03//build/static/js/runtime~main.d77b7384.js"
+  }
+]);
